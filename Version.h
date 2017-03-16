ce160a8dc64a24451133cc8f900c43b7cffe20ef
tchv71 <tchv71@mail.ru>
2017-03-16 13:23:01 +0300

    Ver. 1.11.3.1

--- Version.h
+++ Version.h
@@ -9,25 +9,25 @@
 ////////////////////////////////////////////////////////////////////////////////////
 // Version info
 #ifdef SCAD11
-#define SV_VER				1,11,3,0
-#define SV_VER_STR			"1.11.3.0\0"
+#define SV_VER				1,11,3,1
+#define SV_VER_STR			"1.11.3.1\0"
 #define SV_PRODUCT_VER		11,1,0,0
 #define SV_PRODUCT_VER_STR	"11.1.0.0\0"
 #define SV_PRODUCT_NAME		"Scad Office R11.1\0"
 #else
 #ifdef SCAD21
-#define SV_VER				1,21,3,0
+#define SV_VER				1,21,3,1
 #define SV_VER_STR			"1.21.3.0\0"
 #define SV_PRODUCT_VER		21,1,1,1
 #define SV_PRODUCT_VER_STR	"21.1.1.1\0"
 #define SV_PRODUCT_NAME		"Scad Office R21.1\0"
 #else
-#define SV_VER				1,11,2,0
-#define SV_VER_STR			"1.11.2.0\0"
+#define SV_VER				1,11,3,1
+#define SV_VER_STR			"1.11.3.1\0"
 #define SV_PRODUCT_VER		7,31,5,0
 #define SV_PRODUCT_VER_STR	"7,31,5,0\0"
 #define SV_PRODUCT_NAME		"Scad Office 7.31 R5\0"
 #endif
 #endif
-#define SV_COPYRIGHT		"Copyright (C) 2000-16 Dmitry Tcvetkov\0"
+#define SV_COPYRIGHT		"Copyright (C) 2000-17 Dmitry Tcvetkov\0"
 #endif
